2b1be8958bc97d471403929514a757827e2c9d91
ysicing <root@ysicing.net>
2017-03-17 18:11:56 +0800

    add

--- application/database.php
+++ application/database.php
@@ -13,15 +13,15 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => MYSQL_HOST,
+    'hostname'       => '127.0.0.1',
     // 数据库名
     'database'       => 'thinkblog',
     // 用户名
-    'username'       => MYSQL_USER,
+    'username'       => 'admin'',
     // 密码
-    'password'       => MYSQL_PASS,
+    'password'       => 'a9e1bd65',
     // 端口
-    'hostport'       => MYSQL_PORT,
+    'hostport'       => '3312',,
     // 连接dsn
     'dsn'            => '',
     // 数据库连接参数
