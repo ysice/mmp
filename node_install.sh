4b46ceeb264440adfd6cf0ddf23aa2a65b9afe54
ysicing <root@ysicing.net>
2017-03-24 15:09:23 +0800

    fixed sh

--- node_install.sh
+++ node_install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-nodename=`dc-ctl get node | grep -vE '(manage|name)' | awk '{print $2}' `
-if [ ! -n  $nodename ];then
+nodename=`dc-ctl get node | grep -vE '(name|False|manage|compute)' | awk '{print $2}' `
+if [  -z  $nodename ];then
 	echo "please start dc-agent in compute"
+	exit 0
 else
+	echo $nodename
+	exit 0
 	dc-ctl set node $nodename  --add-identity compute
 	dc-ctl install compute --node $nodename
 fi
--- ubuntu_agent.sh
+++ ubuntu_agent.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -o errexit
 
-if [ -z "$MANAGE_IP" ];then
-	echo "Unknown MANAGE_IP.Please export MANAGE_IP=ip_address(Manage ip)"
-	exit 3
-fi
-
 echo deb http://repo.goodrain.com/ubuntu/14.04 2017.02 main | tee /etc/apt/sources.list.d/goodrain.list  && \
 curl http://repo.goodrain.com/gpg/goodrain-C4CDA0B7 2>/dev/null | apt-key add - && \
 apt-get update
@@ -18,6 +13,6 @@ if [ -f "/var/run/dc-agent.pid" ];then
     pid=`cat /var/run/dc-agent.pid`
     kill -0 $pid || kill -9 $pid
 fi
-gent -d -s http://$MANAGE_IP:4001
+dc-agent -d -s http://$MANAGE_IP:4001
 
 
